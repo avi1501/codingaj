969ea5cba18be1ce4202eda77a2c66b38521abf1
avi1501 <officialaviid@gmail.com>
2021-06-05 18:04:51 +0530

    v1 ready

--- staticfiles/js/home1.js
+++
@@ -1,8 +0,0 @@
-function classToggle() {
-    alert("JS Function");
-    const navs = document.querySelectorAll('.nav__links').style.display="block";
-    
-    // navs.forEach(nav => nav.classList.toggle('nav__links'));
-  }
-  
-  document.getElementsByClassName('.menu__button').addEventListener('click', classToggle);
